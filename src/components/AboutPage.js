9b2499c422afb566f82f620f421a9ceddcaa9bb9
Jordan Peck <jpeckwriter@gmail.com>
2021-07-01 15:29:46 -0600

    Course list now renders.

---
+++ src/components/AboutPage.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+class AboutPage extends React.Component{
+    render() {
+        return(
+            <>
+        <h2>About</h2>
+        <p>This app is special</p>
+        </>
+        ) 
+        
+    }
+}
+
+export default AboutPage;
